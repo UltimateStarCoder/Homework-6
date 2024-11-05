bb42484fff9a64330f0d2005361be9f2a3811431
Mac-Rufus Umeokolo <onymacr@gmail.com>
2024-11-05 09:43:04 -0700

    Final. Now I can print the grid even when there is an error.

--- Question3.py
+++ Question3.py
@@ -2,18 +2,19 @@ from Grid_info import grid1, grid2, grid3
 from Grid_info import grid1_blockade, grid2_blockade, grid3_blockade
 
 def print_grid(gridToPrint):
+    """Prints grid in readable format."""
     for row in gridToPrint:
         print(" ".join(map(str, row)))
 
-
 def robot_coin_collection(Coin, blockade):
-    n = len(Coin)      # Number of rows
-    m = len(Coin[0])   # Number of columns
+    """Calculates maximum coins collectible in grid with blockades."""
+    n = len(Coin)
+    m = len(Coin[0])
 
-    if blockade[0][0] == 'x':
-        return "Starting point is unreachable"
+    if blockade[0][0] == 'x' or blockade[n-1][m-1] == 'x':
+        return "Starting or ending point is unreachable"
 
-    # Populate F with 0 values for all cells to match the Coin grid size
+    # Populate F with zeros
     F = [[0] * m for _ in range(n)]
     F[0][0] = Coin[0][0]
 
@@ -23,13 +24,14 @@ def robot_coin_collection(Coin, blockade):
             F[i][0] = F[i-1][0] + Coin[i][0]
         else:
             F[i][0] = "x"
-        
-        # Fill rest of the grid
+
+    # Fill rest of grid
+    for i in range(n):
         for j in range(1, m):
             if blockade[i][j] != 'x':
-                if F[i-1][j] != "x" and F[i][j-1] != "x":
+                if i > 0 and F[i-1][j] != "x" and F[i][j-1] != "x":
                     F[i][j] = max(F[i-1][j], F[i][j-1]) + Coin[i][j]
-                elif F[i-1][j] != "x":
+                elif i > 0 and F[i-1][j] != "x":
                     F[i][j] = F[i-1][j] + Coin[i][j]
                 elif F[i][j-1] != "x":
                     F[i][j] = F[i][j-1] + Coin[i][j]
@@ -37,21 +39,39 @@ def robot_coin_collection(Coin, blockade):
                     F[i][j] = "x"
             else:
                 F[i][j] = "x"
-    
-    #print_grid(F)
 
     if F[n-1][m-1] == "x":
-        return "End point is unreachable"
+        return "End point is unreachable", F
     else:
         return F[n-1][m-1], F
-    
-    
+
 if __name__ == "__main__":
-    max_coins, _ = robot_coin_collection(grid1, grid1_blockade)
-    print("The maximum number of coins you collected is:", max_coins)
+    result1 = robot_coin_collection(grid1, grid1_blockade)
+    if isinstance(result1, str):
+        print(result1)
+        _, max_coin_grid1 = result1
+        print_grid(max_coin_grid1)
+    else:
+        max_coin1, max_coin_grid1 = result1
+        print("The maximum number of coins you collected:", max_coin1)
+        print_grid(max_coin_grid1)
 
-    max_coins2, _ = robot_coin_collection(grid2, grid2_blockade)
-    print("The maximum number of coins you collected is:", max_coins2)
+    result2 = robot_coin_collection(grid2, grid2_blockade)
+    if isinstance(result2, str):
+        print(result2)
+        _, max_coin_grid2 = result2
+        print_grid(max_coin_grid2)
+    else:
+        max_coin2, max_coin_grid2 = result2
+        print("The maximum number of coins you collected:", max_coin2)
+        print_grid(max_coin_grid2)
 
-    max_coins3, _ = robot_coin_collection(grid3, grid3_blockade)
-    print("The maximum number of coins you collected is:", max_coins3)
+    result3 = robot_coin_collection(grid3, grid3_blockade)
+    if isinstance(result3, str):
+        print(result3)
+        _, max_coin_grid3 = result3
+        print_grid(max_coin_grid3)
+    else:
+        max_coin3, max_coin_grid3 = result3
+        print("The maximum number of coins you collected:", max_coin3)
+        print_grid(max_coin_grid3)
